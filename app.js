7034a44d4ec36163ee7ac52279efc6255e5854ac
Ryan Roat <ryanroat@gmail.com>
2020-05-08 17:11:39 -0400

    config json files and .gitignore update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.json({ extended: false }));
 const localPort = 5002;
 
 app.listen(process.env.PORT || localPort, () =>
+    // eslint-disable-next-line no-console
     console.log(`Server started on port ${localPort}.`)
 );
 
