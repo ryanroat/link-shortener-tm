13f96f1cfb1cfa69d893577d73749887e4250402
Ryan Roat <ryanroat@gmail.com>
2020-05-08 16:39:58 -0400

    start express server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('working');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+const localPort = 5002;
+
+app.listen(process.env.PORT || localPort, () =>
+    console.log(`Server started on port ${localPort}.`)
+);
+
+// console.log('working'); TODO: remove for production
