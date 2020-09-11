0db5bdb25cfd2df724d8c32f0a4399c12702ad0b
Ryan Roat <ryanroat@gmail.com>
2020-09-11 11:53:28 -0400

    fix eslint warnings

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,31 +1,32 @@
+/* eslint-disable linebreak-style */
 const mongoose = require('mongoose');
 const shortId = require('shortid');
 const config = require('config');
 
 const shortUrlSchema = new mongoose.Schema({
-    target: {
-        type: String,
-        required: true
-    },
-    domainRoot: {
-        type: String,
-        required: true,
-        default: config.get('domainRoot')
-    },
-    short: {
-        type: String,
-        required: true,
-        default: shortId.generate
-    },
-    clicks: {
-        type: Number,
-        required: true,
-        default: 0
-    },
-    createdDate: {
-        type: Date,
-        default: Date.now
-    }
+  target: {
+    type: String,
+    required: true,
+  },
+  domainRoot: {
+    type: String,
+    required: true,
+    default: config.get('domainRoot'),
+  },
+  short: {
+    type: String,
+    required: true,
+    default: shortId.generate,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('shortUrl', shortUrlSchema);
