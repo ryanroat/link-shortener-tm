440039d057d50928b3af699f9b150f551afc07cb
Ryan Roat <ryanroat@gmail.com>
2020-09-12 14:28:30 -0400

    store/set darkmode via local storage

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -1,9 +1,28 @@
+/* eslint-disable linebreak-style */
 /*
 based off https://dev.to/ekaterina_vu/dark-mode-with-one-line-of-code-4lkm
 and https://www.youtube.com/watch?v=qimopjP6YoM
 */
 
+const setDM = () => {
+  const darkmodeOn = window.localStorage.getItem('darkmode');
+  if (darkmodeOn === 'true') {
+    document.documentElement.classList.add('dark-mode');
+  } else {
+    document.documentElement.classList.remove('dark-mode');
+  }
+  console.log('darkmode status: ', darkmodeOn);
+};
+
+const toggleDM = () => {
+  const darkmodeOn = window.localStorage.getItem('darkmode') === 'true' ? 'false' : 'true';
+  window.localStorage.setItem('darkmode', darkmodeOn);
+  setDM();
+};
+
+setDM();
 const darkmode = document.querySelector('#darkmode');
 darkmode.addEventListener('click', () => {
-  document.documentElement.classList.toggle('dark-mode');
+  // document.documentElement.classList.toggle('dark-mode');
+  toggleDM();
 });
